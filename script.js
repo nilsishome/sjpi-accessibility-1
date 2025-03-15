a07d799129b0ee16745a8e07e9e1643c8b4ee114
nilsishome <nilsnyberg24@gmail.com>
2025-03-15 11:52:20 +0100

    Initial commit, comment out and change unnecessary scripts in script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Simulerad blockering av tråden
-setInterval(() => {
+/* setInterval(() => {
   let start = Date.now()
   while (Date.now() - start < 1000) {}
-}, 3000)
+}, 3000) */
 
 // Skriver ut HTML direkt i dokumentet
-document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
-document.write("<p>Another document.write() call</p>")
+/* document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
+document.write("<p>Another document.write() call</p>") */
 
 // Förhindrar användaren att navigera bakåt
-history.pushState(null, "", location.href)
-window.onpopstate = () => history.go(1)
+/* history.pushState(null, "", location.href)
+window.onpopstate = () => history.go(1) */
 
 // Ändrar bakgrundsfärgen slumpmässigt var 500ms
 setInterval(() => {
@@ -18,17 +18,15 @@ setInterval(() => {
     "#" + Math.floor(Math.random() * 16777215).toString(16)
 }, 500)
 
-// Visar innehållet på sidan efter 5 sekunder
-setTimeout(() => {
-  document.body.style.display = "block"
-}, 5000)
+// Visar innehållet på sidan efter 5 sekunder (ändrat så det visas direkt istället)
+document.body.style.display = "block"
 
 // Loggar 10000 tal till konsolen efter 1 sekund
-setTimeout(() => {
+/* setTimeout(() => {
   for (let i = 0; i < 10000; i++) {
     console.log(i)
   }
-}, 1000)
+}, 1000) */
 
 // // Visar en alert när sidan har laddats
 // document.addEventListener("DOMContentLoaded", function () {
@@ -36,6 +34,6 @@ setTimeout(() => {
 // })
 
 // Loggar 100 000 siffror direkt när sidan laddas
-for (let i = 0; i < 100000; i++) {
+/* for (let i = 0; i < 100000; i++) {
   console.log(i)
-}
+} */
